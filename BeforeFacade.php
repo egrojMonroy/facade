bb2e514323ba618ccdf3f91fc2b77e68dc71e959
jorge <egrojmonroy@gmail.com>
2017-05-21 20:48:25 -0400

    Creacion de clases php

---
+++ BeforeFacade.php
@@ -0,0 +1,11 @@
+<?php
+
+class Banco {
+
+}
+class Credito{
+
+}
+class Prestamo{
+
+}
---
+++ facadeHipoteca.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jorge Monroy
+ * Date: 21/05/2017
+ * Time: 08:38 PM
+ */
+class facadeHipoteca
+{
+    public function __construct(Banco $banco, Prestamo $prestamo,Credito $credito)
+    {
+        $this->_banco = $banco;
+        $this->_prestamo = $prestamo;
+        $this->_credito = $credito;
+    }
+
+    public function esFiable ($cliente){
+        if($this->_banco->tieneFondos($cliente) && $this->_credito->historialCorrecto($cliente) && $this->prestamo->tienePrestamo($cliente) ){
+
+        }else{
+
+        }
+    }
+}
